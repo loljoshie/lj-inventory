867a83f764d8f4d64150ba4f9c2e4fc115390c73
aj <66404074+ihyajb@users.noreply.github.com>
2022-08-25 07:33:54 -0400

    fix: Forgot the manifest lol

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,10 @@ description 'lj-Inventory'
 version '1.0'
 
 shared_scripts {
-	'config.lua',
-	'@qb-weapons/config.lua'
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua', -- Change to the language you want
+    'config.lua',
+    '@qb-weapons/config.lua'
 }
 
 server_scripts {
