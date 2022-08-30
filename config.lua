f3e4f51af0874a39234eb0bdbb04c84a25f41b39
Randolio <103154494+Randolio@users.noreply.github.com>
2022-08-30 19:18:06 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,15 @@
 Config = {}
 
+Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
+
+Config.MaxInventoryWeight = 120000 -- Max weight a player can carry (default 120kg, written in grams)
+Config.MaxInventorySlots = 41 -- Max inventory slots for a player
+
+Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
+Config.MaxDropViewDistance = 12.5 -- The distance in GTA Units that a drop can be seen
+Config.UseItemDrop = false -- This will enable item object to spawn on drops instead of markers
+Config.ItemDropObject = `sf_prop_sf_backpack_01a` -- if Config.UseItemDrop is true, this will be the prop that spawns for the item
+
 Config.VendingObjects = {
     "prop_vend_soda_01",
     "prop_vend_soda_02",
@@ -322,8 +332,6 @@ Config.AttachmentCrafting = {
         },
     }
 }
-
-MaxInventorySlots = 41
 
 BackEngineVehicles = {
     [`ninef`] = true,
